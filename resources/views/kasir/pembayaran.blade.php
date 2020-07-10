02aca0f55dcf76c2e08c3cacf5d4c1f4ee0325f9
Darma Wiryanata <45613994+ScrawnyGus@users.noreply.github.com>
2020-07-10 11:47:19 +0800

    Pembayaran card size, tambah button, trash button

--- resources/views/kasir/pembayaran.blade.php
+++ resources/views/kasir/pembayaran.blade.php
@@ -14,7 +14,7 @@
 @section('content')
     <!-- Basic Tables start -->
       <div class="row" id="basic-table">
-        <div class="col-12">
+        <div class="col-md-6 col-12 ml-auto mr-auto">
             <div class="card">
                 <div class="card-header">
                     <h4 class="card-title">Detail Pembayaran</h4>
@@ -22,12 +22,12 @@
                 <div class="card-content">
                     <div class="card-body">
                       <div class="row justify-content-center">
-                        <div class="col-8">
+                        <div class="col-12">
                           <div class="table-responsive">
                               <table class="table">
                                 <thead>
                                     <th style="width:70%">Item</th>
-                                    <th style="width:30%">Biaya</th>
+                                    <th colspan="2" style="width:30%">Biaya</th>
                                 </thead>
                                 <tbody>
                                     <tr>
@@ -36,26 +36,34 @@
                                         <span class="ml-2">x 1</span>
                                         </td>
                                         <td>Rp. 25.000</td>
+                                        <td><button type="button" style="padding: 0; border: none; background: none;" class="action-edit text-danger" ><i class="feather icon-trash"></i></span></td>
                                     </tr>
                                     <tr>
                                         <td>Kopi Leak Kaleng<br>
                                         <span class="ml-2">x 2</span>
                                         </td>
                                         <td>Rp. 29.000</td>
+                                        <td><button type="button" style="padding: 0; border: none; background: none;" class="action-edit text-danger" ><i class="feather icon-trash"></i></span></td>
                                     </tr>
                                     <tr>
                                         <td>Kopi Kita Serasi<br>
                                         <span class="ml-2">x 2</span></td>
                                         <td>Rp. 15.000</td>
+                                        <td><button type="button" style="padding: 0; border: none; background: none;" class="action-edit text-danger" ><i class="feather icon-trash"></i></span></td>
                                     </tr>
                                     <tr>
                                         <td class="text-right text-bold">TOTAL</td>
                                         <td>Rp. 69.000</td>
                                     </tr>
                                     <tr>
-                                        <td class="text-right"></td>
                                         <td>
-                                            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#bayar" data-value="#">Bayar</button>
+                                            <a href="{{ route('kasir.order') }}">
+                                                <button type="button" class="btn btn-primary">Tambah</button>
+                                            </a>
+                                        </td>
+                                        {{-- <td class="text-right"></td> --}}
+                                        <td>
+                                            <button type="button" class="btn btn-success float-right" data-toggle="modal" data-target="#bayar" data-value="#">Bayar</button>
                                         </td>
                                     </tr>
                                 </tbody>
