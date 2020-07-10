38f8b8637d30999e62fe96912c5900225549dc4a
Darma Wiryatama <wirya_tama99@hotmail.com>
2020-07-10 16:06:28 +0800

    Navigasi Order

--- resources/views/kasir/order.blade.php
+++ resources/views/kasir/order.blade.php
@@ -24,126 +24,112 @@
               </div>
               <div class="card-content">
                 <div class="card-body">
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="card border-primary text-center bg-transparent">
-                            <div class="card-content d-flex">
-                                <div class="card-body">
-                                    <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
-                                    <p class="card-text">Donut toffee candy brownie.</p>
-                                    <div class="row justify-content-center">
-                                      <div class="col-3">
-                                        <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
-                                      </div>
+                  <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item">
+                      <a class="nav-link active" id="kopi-panas-tab" data-toggle="tab" href="#kopi-panas" role="tab" aria-controls="home" aria-selected="true">Kopi Panas</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="kopi-dingin-tab" data-toggle="tab" href="#kopi-dingin" role="tab" aria-controls="profile" aria-selected="false">Kopi Dingin</a>
+                    </li>
+                  </ul>
+                  <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="kopi-panas" role="tabpanel" aria-labelledby="home-tab">
+                      <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-6">
+                            <div class="card border-primary text-center bg-transparent">
+                                <div class="card-content d-flex">
+                                    <div class="card-body">
+                                        <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
+                                        <p class="card-text">Donut toffee candy brownie.</p>
+                                        <div class="row justify-content-center">
+                                          <div class="col-3">
+                                            <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
+                                          </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6">
+                            <div class="card border-primary text-center bg-transparent">
+                                <div class="card-content d-flex">
+                                    <div class="card-body">
+                                        <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
+                                        <p class="card-text">Donut toffee candy brownie.</p>
+                                        <div class="row justify-content-center">
+                                          <div class="col-3">
+                                            <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
+                                          </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6">
+                            <div class="card border-primary text-center bg-transparent">
+                                <div class="card-content d-flex">
+                                    <div class="card-body">
+                                        <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
+                                        <p class="card-text">Donut toffee candy brownie.</p>
+                                        <div class="row justify-content-center">
+                                          <div class="col-3">
+                                            <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
+                                          </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
                     </div>
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="card border-primary text-center bg-transparent">
-                            <div class="card-content d-flex">
-                                <div class="card-body">
-                                    <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
-                                    <p class="card-text">Donut toffee candy brownie.</p>
-                                    <div class="row justify-content-center">
-                                      <div class="col-3">
-                                        <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
-                                      </div>
+                    <div class="tab-pane fade" id="kopi-dingin" role="tabpanel" aria-labelledby="kopi-dingin-tab">
+                      <div class="row">
+                        <div class="col-lg-3 col-md-4 col-sm-6">
+                            <div class="card border-primary text-center bg-transparent">
+                                <div class="card-content d-flex">
+                                    <div class="card-body">
+                                        <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
+                                        <p class="card-text">Doasdnut toffee candy brownie.</p>
+                                        <div class="row justify-content-center">
+                                          <div class="col-3">
+                                            <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
+                                          </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="card border-primary text-center bg-transparent">
-                            <div class="card-content d-flex">
-                                <div class="card-body">
-                                    <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
-                                    <p class="card-text">Donut toffee candy brownie.</p>
-                                    <div class="row justify-content-center">
-                                      <div class="col-3">
-                                        <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
-                                      </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6">
+                            <div class="card border-primary text-center bg-transparent">
+                                <div class="card-content d-flex">
+                                    <div class="card-body">
+                                        <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
+                                        <p class="card-text">Donuasdt toffee candy brownie.</p>
+                                        <div class="row justify-content-center">
+                                          <div class="col-3">
+                                            <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
+                                          </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="card border-primary text-center bg-transparent">
-                            <div class="card-content d-flex">
-                                <div class="card-body">
-                                    <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
-                                    <p class="card-text">Donut toffee candy brownie.</p>
-                                    <div class="row justify-content-center">
-                                      <div class="col-3">
-                                        <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
-                                      </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6">
+                            <div class="card border-primary text-center bg-transparent">
+                                <div class="card-content d-flex">
+                                    <div class="card-body">
+                                        <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
+                                        <p class="card-text">Donuasdt toffee candy brownie.</p>
+                                        <div class="row justify-content-center">
+                                          <div class="col-3">
+                                            <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
+                                          </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="card border-primary text-center bg-transparent">
-                            <div class="card-content d-flex">
-                                <div class="card-body">
-                                    <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
-                                    <p class="card-text">Donut toffee candy brownie.</p>
-                                    <div class="row justify-content-center">
-                                      <div class="col-3">
-                                        <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
-                                      </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="card border-primary text-center bg-transparent">
-                            <div class="card-content d-flex">
-                                <div class="card-body">
-                                    <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
-                                    <p class="card-text">Donut toffee candy brownie.</p>
-                                    <div class="row justify-content-center">
-                                      <div class="col-3">
-                                        <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
-                                      </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="card border-primary text-center bg-transparent">
-                            <div class="card-content d-flex">
-                                <div class="card-body">
-                                    <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
-                                    <p class="card-text">Donut toffee candy brownie.</p>
-                                    <div class="row justify-content-center">
-                                      <div class="col-3">
-                                        <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
-                                      </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-6">
-                        <div class="card border-primary text-center bg-transparent">
-                            <div class="card-content d-flex">
-                                <div class="card-body">
-                                    <img src="../../../app-assets/images/elements/ipad-pro.png" alt="element 06" height="150" class="mb-1">
-                                    <p class="card-text">Donut toffee candy brownie.</p>
-                                    <div class="row justify-content-center">
-                                      <div class="col-3">
-                                        <input type="number" class="form-control text-center" name="#" id="#" minlength="1" min="1" max="99" placeholder="0">
-                                      </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
